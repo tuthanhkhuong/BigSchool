bb1e4e6c1423e35dca5c14379b7dfab4b88e9e45
Administrator <Administrator@PC206>
2020-06-12 13:26:39 +0700

    Loi Create

--- Web_Lab456/Models/Course.cs
+++ Web_Lab456/Models/Course.cs
@@ -11,19 +11,19 @@ namespace Web_Lab456.Models
         public int Id { get; set; }
 
         public ApplicationUser Lecturer { get; set; }
-        [Required]
 
+        [Required]
         public string LecturerId { get; set; }
+
         [Required]
         [StringLength(255)]
-
         public string Place { get; set; }
 
         public DateTime DateTime { get; set; }
 
         public Category Category { get; set; }
-        [Required]
 
+        [Required]
         public byte CategoryId { get; set; }
 
         
