1260bec403338bcc4667fb4a37563258e8064a66
tuthanhkhuong@gmail.com <tuthanhkhuong@gmail.com>
2020-06-18 21:44:53 +0700

    Mau trang

---
+++ Web_Lab456/Migrations/202006181431409_InitialModel2.cs
@@ -0,0 +1,18 @@
+namespace Web_Lab456.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "IsCanceled", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Courses", "IsCanceled");
+        }
+    }
+}
